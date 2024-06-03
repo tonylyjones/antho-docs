188c03277855a86581093f3e7867434c382fc89c
Anthony Ly <anthonyly.dev@gmail.com>
2024-06-03 11:11:14 +0200

    ui: og banner

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/og-banner.jpg",
     navbar: {
       title: "Anthony Ly",
       logo: {
